22cc043586e27372cf9c1685abd8e50718e26b7e
Rahul Bir <rahul@rahulbir.com>
2016-10-16 12:17:45 -0600

    Cleaned code. Cached images. Added asynchronous search requests.

--- assets/js/photogallery.js
+++ assets/js/photogallery.js
@@ -1,88 +1,183 @@
-const googleApi = {
-  host:             'https://www.googleapis.com/customsearch/v1',
-  key:              'AIzaSyAFwXrSDLvjyqyZbLlZ_csNiCkDcsAHDeU',
-  searchEngineId:   '013872130897346659415:yae-br_hnby',
-  imageSize:        'xlarge',
-  searchType:       'image',
-  num:              '10'
-}
+(function() {
+  // dom elements 
+  var photoGallery     =  document.getElementById('photo-gallery');
+  var lightbox         =  document.getElementById('lightbox');
+  var lightboxImage    =  document.getElementById('lightbox-image');
+  var lightboxCaption  =  document.getElementById('lightbox-caption');
+  var lightboxPrevBtn  =  document.getElementById('prev-lightbox-btn');
+  var lightboxNextBtn  =  document.getElementById('next-lightbox-btn');
+  var lightboxCloseBtn =  document.getElementById('close-lightbox-btn');
+  
+  // global variables
+  var images = [];                // meta data for images obtained from Google Search API
+  var currentLightboxIndex = 0;   // index of the current image displayed in the lightbox
+  var imageCount = 0;             // total number of images created. Used to track id's of images.
+  var googleApi = {               // config for Google Search API
+    host:             'https://www.googleapis.com/customsearch/v1',
+    key:              'AIzaSyAFwXrSDLvjyqyZbLlZ_csNiCkDcsAHDeU',
+    searchEngineId:   '013872130897346659415:yae-br_hnby',
+    imageSize:        'xxlarge',
+    searchType:       'image',
+    num:              '10'
+  };
 
-// INITIALIZER
-var images = [];
-var photoGallery = document.getElementById('photos');
-var lightbox = document.getElementById('lightbox');
-var lightboxImage = document.getElementById('lightbox-image');
-var lightboxCaption = document.getElementById('lightbox-caption');
-var currentImageIndex = 0;
+  // on document load 
+  getImageData('wallpaper', 1);
+  getImageData('san+francisco', 2);
+  initLightboxView();
 
-// TODO Change this to use asynchronous call
-function getImages(search, page) {
-  page = (page && (typeof page === 'integer')) ? page : 1;
-  var xhr = new XMLHttpRequest();
-  var path = '?q=' + search + 
-             '&cx=' + googleApi.searchEngineId +
-             '&key=' + googleApi.key +
-             '&imgSize=' + googleApi.imageSize +
-             '&searchType=' + googleApi.searchType +
-             '&num=' + googleApi.num + 
-             '&start=' + page;
+  /**
+   * Retrieve images from Google Search API asynchronously and initializes each image
+   *
+   * @param  {String} search    'Wallpaper'
+   * @param  {Integer} page     defaults to 1
+   */
+  function getImageData(search, page) {
+    search = search ? search : 'wallpaper';
+    page = (page && (typeof page === 'integer')) ? page : 1;
+    var path = '?q=' + search + 
+               '&cx=' + googleApi.searchEngineId +
+               '&key=' + googleApi.key +
+               '&imgSize=' + googleApi.imageSize +
+               '&searchType=' + googleApi.searchType +
+               '&num=' + googleApi.num + 
+               '&start=' + page;
 
-  xhr.open('GET', `${googleApi.host}${path}`, false);
-  xhr.send(null);
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(e) {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        response = JSON.parse(xhr.responseText);
+        images = images.concat(response.items);
 
-  response = JSON.parse(xhr.response);
+        response.items.map(function(image) {
+          initThumbnail(imageCount);
+          initImage(image, imageCount);
+          imageCount++;
+        });
+      }
+    };
 
-  if(response.error) {
-    return [];
+    xhr.onerror = function (e) {
+      console.error(xhr.statusText);
+    };
+
+    xhr.open('GET', `${googleApi.host}${path}`);
+    xhr.send(null);
+  };
+
+  /**
+   * Create and initialize thumbnail with id
+   *
+   * @param  {Integer} id       id of the image associated with this thumbnail
+   */
+  function initThumbnail(id) {    
+    var thumbnail = document.createElement('div');
+    thumbnail.className = 'thumbnail';
+    thumbnail.setAttribute('data-id', id);
+    photoGallery.appendChild(thumbnail);
   }
 
-  return response.items;
-};
+  /**
+   * Create, initialize, and preload images. 
+   * Display images on screen once loaded. 
+   * 
+   * @note: This will cache all images in browser once loaded.
+   *
+   * @param  {Object} image     image meta data from google search api
+   * @param  {Integer} id       id of the image
+   */
+  function initImage(image, id) {
+    var thumbnail = document.querySelector('.thumbnail[data-id="' + id + '"]');
+    startSpinner(thumbnail);
 
-function findImages() {
-  var search = document.forms['search-form']['search'].value;
-  
-  var data = '';
-  
-  images = getImages(search, 1);
-  images = images.concat(getImages(search, 2));
+    var imageContainer = new Image();
+    
+    imageContainer.onload = function() {
+      thumbnail.appendChild(imageContainer);
+      stopSpinner(thumbnail);
+    }
 
-  for (var i = 0; i < images.length; i++) {
-    data += '<div class="thumbnail"><img src="' + images[i].link + '" onclick="handleImageClick(' + i + ')"></img></div>';
+    imageContainer.src = image.link;
+    imageContainer.onclick = function() {
+      displayLightboxView(id);
+    };
   }
 
-  photoGallery.innerHTML = data;
-  return false;
-}
+  function initLightboxView() {
+    lightboxPrevBtn.onclick = function() {
+      navigateLightbox(-1);
+    };
 
-function handleImageClick(index) {
-  currentImageIndex = index; 
-  lightbox.classList.remove('hide');
-  lightboxImage.src = images[index].link;
-  lightboxCaption.innerHTML = images[index].title;
-}
+    lightboxNextBtn.onclick = function() {
+      navigateLightbox(1);
+    };
 
-function navigate(direction) {
-  currentImageIndex += direction;
+    lightboxCloseBtn.onclick = function() {
+      closeLightbox();
+    };
+  }
 
-  if (currentImageIndex > images.length - 1) { currentImageIndex = 0; }
-  if (currentImageIndex < 0) { currentImageIndex = images.length - 1; }
+  /**
+   * Displays clicked image thumbnail in lightbox view
+   *
+   * @param  {Integer} index
+   */
+  function displayLightboxView(index) {
+    currentLightboxIndex = index; 
+    lightbox.classList.remove('hide');
+    lightboxImage.src = images[index].link;
+    lightboxCaption.innerHTML = images[index].title;
+  }
 
-  handleImageClick(currentImageIndex);
-}
+  /**
+   * Changes the lightbox image to the next or previous image in the photo-gallery list
+   *
+   * @param  {Integer} direction     -1 for previos, 1 for next
+   */
+  function navigateLightbox(direction) {
+    if(direction !== -1 && direction !== 1) { return; }
 
-function closeLightbox() {
-  lightbox.classList.add('hide');
-  lightboxImage.src = '';
-  lightboxCaption.innerHTML = '';
-}
+    lightboxImage.src = '';
 
+    currentLightboxIndex += direction;
+    if (currentLightboxIndex > images.length - 1) { currentLightboxIndex = 0; }
+    if (currentLightboxIndex < 0) { currentLightboxIndex = images.length - 1; }
 
+    displayLightboxView(currentLightboxIndex);
+  }
 
+  /**
+   * Closes the lightbox view
+   */
+  function closeLightbox() {
+    lightbox.classList.add('hide');
+    lightboxImage.src = '';
+    lightboxCaption.innerHTML = '';
+  }
 
+  /**
+   * Adds a loading spinner to target element 
+   *
+   * @param  {document.Element} targetElement
+   */
+  function startSpinner(targetElement) {
+    if(!targetElement) { return; }
 
+    var spinner = document.createElement('div');
+    spinner.className = 'spinner';
 
+    targetElement.appendChild(spinner);
+  }
 
+  /**
+   * Stops a loading spinner on a target element 
+   *
+   * @param  {document.Element} targetElement
+   */
+  function stopSpinner(targetElement) {
+    if(!targetElement) { return; }
 
-
-
+    var spinner = document.querySelector('.thumbnail[data-id="' + targetElement.getAttribute('data-id') + '"] div.spinner');
+    targetElement.removeChild(spinner);
+  }
+})();
